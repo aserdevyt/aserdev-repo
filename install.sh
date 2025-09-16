30c50b639c2b581092e4fc6648855e0c18bde7e9
aserdev <aserdevyt@outlook.com>
2025-09-16 19:00:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,20 @@ ARCH=$(uname -m)  # usually x86_64
 PACMAN_CONF="/etc/pacman.conf"
 REPO_URL="https://raw.githubusercontent.com/$GITHUB_USER/aserdev-repo/main/$ARCH"
 
+echo "==> installing choatic ar for depends"
+
+# add signing key
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+# install the keyring,mirrorlist
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# add chaotic aur repo
+echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist'   | sudo tee -a /etc/pacman.conf
+
+# update
+sudo pacman -Syu --noconfirm
 # ----------------------------
 # Add the repo to pacman.conf
 # ----------------------------
